d287ea51d9d87c00cc73ff6b576a7e1a1dbbff7c
user.name like REIMOC96 <reinaldo.marcelo.ordonez@cftmail.cl>
2024-02-19 20:01:38 -0300

    XD

--- ELCAREDCOM/src/app/app.routes.ts
+++ ELCAREDCOM/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { PrincBrotherComponent } from './princ-brother/princ-brother.component';
 
-export const routes: Routes = [];
+export const routes: Routes = ([
+    {path:'brother/', component: PrincBrotherComponent},
+])
